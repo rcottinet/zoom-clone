6ae8a7881ac63d503bbdfd4a525e9370fc5fb1ae
Roman Cottinet <romancottinet@gmail.com>
2020-08-16 18:49:27 +0200

    change server peerjs

--- public/script.js
+++ public/script.js
@@ -1,8 +1,8 @@
 const socket = io("/");
 const videoGrid = document.getElementById("video-grid");
 const myPeer = new Peer(undefined, {
-  host: "/",
-  port: "3001",
+  host: "peerjs-server-basic.herokuapp.com",
+  port: "36311",
 });
 const myVideo = document.createElement("video");
 myVideo.muted = true;
